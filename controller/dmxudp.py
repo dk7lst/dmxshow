1fdcbb5512c0687de20ad55ac03b41078572928c
Lars Struss <mail@lsnet.eu>
2020-11-28 18:43:40 +0100

    clear() for dmxudp

--- controller/dmxudp.py
+++ controller/dmxudp.py
@@ -10,11 +10,14 @@ class DMXUDP:
     self.reset()
 
   def reset(self):
-    for i in range(0, 512):
-      self.dmxbuf[i] = 0
+    self.clear()
     self.flush()
 
+  def clear(self):
+    for i in range(0, 512): self.dmxbuf[i] = 0
+
   def set(self, ch, val):
+    val = int(val)
     assert ch >= 1 and ch <= 512
     assert val >= 0 and val <= 255
     self.dmxbuf[ch - 1] = val
